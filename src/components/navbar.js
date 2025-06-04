cd21543cf577233ab8c8de7ab7ee0ee50016a49c
GherardoFiori <B00144205@mytudublin.ie>
2025-06-04 16:02:19 +0000

    Added a lot for phone mainly hamburger

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,25 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <div classname="nav-logo">
-         <Link to="/" className="flex items-center space-x-2">
-        <img src="/logo.png" alt="Buio Logo" className="nav-logo" />
-      </Link>
+      <div className="nav-logo">
+        <Link to="/" className="flex items-center space-x-2">
+          <img src="/logo.png" alt="Buio Logo" className="nav-logo-img" />
+        </Link>
       </div>
-      <div className="links">
-        <Link to="/">HOME</Link>
-        <Link to="/about">ABOUT ME</Link>
-        <Link to="/projects">PROJECTS</Link>
+
+      <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+
+      <div className={`links ${menuOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>HOME</Link>
+        <Link to="/about" onClick={() => setMenuOpen(false)}>ABOUT ME</Link>
+        <Link to="/projects" onClick={() => setMenuOpen(false)}>PROJECTS</Link>
       </div>
     </nav>
   );
